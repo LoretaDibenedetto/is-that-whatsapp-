3d909c708f2d8eab78cdcbf0e751b7b73d43a684
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2024-01-04 12:53:28 +0100

    Auth api

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import {FcGoogle} from "react-icons/fc";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import { CHECk_USER_ROUTE } from "@/utils/ApiRoutes";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 function login() {
-  const handleLogin = ()=>{
-    alert("Login Successful");
+  const router = useRouter()
+
+  const handleLogin = async ()=>{
+    const provider = new GoogleAuthProvider();
+    const {user:{displayName:name,email,photoUrl:profileImage},
+  } = await signInWithPopup(firebaseAuth, provider);
+  try{
+   if(email) {
+
+    const {data} = await axios.post(CHECK_USER_ROUTE, {email});
+    console.log({data});
+
+    if(!data.status) {
+      router.push("/onboarding");
+    }
+   }
+  }catch(error){
+    console.log(error);
+  }
+   
   }
   return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
     <div className="flex items-center justify-center gap-2 text-white">
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import AuthRoutes from "./routes/AuthRoutes.js";
+
 
 dotenv.config();
 const app = express();
@@ -9,8 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", AuthRoutes)
 
-
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT , () => {
     console.log(`Server running on port ${server.address().port}`);
 });
+
