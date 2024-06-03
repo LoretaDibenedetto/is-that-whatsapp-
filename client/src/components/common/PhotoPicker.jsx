e593465ef2e1563304634b0551ba72a4c9047ef4
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2024-06-03 18:11:10 +0200

    fix upload photo

--- client/src/components/common/PhotoPicker.jsx
+++ client/src/components/common/PhotoPicker.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 function PhotoPicker({onChange}) {
   const component =(
 
    <input type= "file" hidden id= "photo-picker" onChange={onChange}/>
   );
-  return ReactDOM.createPortal(component,document.getElementById("photo-picker-element")
+  return ReactDOM.createPortal(
+    component,
+    document.getElementById("photo-picker-element")
 );
 }
 
